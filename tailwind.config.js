aea1c4806935d6d13fed84111e0d6992b9e70029
Swôth <56405862+Swothh@users.noreply.github.com>
2021-06-19 23:52:16 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,5 @@
 module.exports = {
     purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
     darkMode: false,
-    theme: {
-        extend: {}
-    },
-    variants: {
-        extend: {}
-    },
     plugins: []
 };
